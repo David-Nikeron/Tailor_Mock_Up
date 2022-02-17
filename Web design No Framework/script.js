9eb5526f6a6ae7a8ada4978ab2a1332c45c49099
David-Nikeron <99284001+David-Nikeron@users.noreply.github.com>
2022-02-17 01:42:29 +0100

    Add files via upload

---
+++ Web design No Framework/script.js
@@ -0,0 +1,50 @@
+
+
+(function () {
+  "use strict";
+
+        // imagesSource = [
+        //     "/home/solomon/Web design No Framework/Screenshot from 2021-03-18 17-56-04.png",
+        //     "/home/solomon/Web design No Framework/Screenshot from 2021-03-23 14-35-05.png",
+        //     "/home/solomon/Web design No Framework/Screenshot from 2021-03-23 15-51-56.png",
+        //     "/home/solomon/Web design No Framework/Screenshot from 2021-03-29 19-06-41.png"
+        // ]
+
+        // currentImage = 0;
+
+        // document.getElementById('next').onclick = nextPhoto
+
+        // function nextPhoto() {
+        //     currentImage += 1;
+        //     if (currentImage === imagesSource.length) {
+        //         currentImage = 0
+
+
+        //     }
+
+        //     document.getElementById('image').src = imagesSource[currentImage]
+        // }
+
+
+        // document.getElementById('prev').onclick = previousPhoto
+
+        // function previousPhoto() {
+        //     currentImage -= 1
+
+
+        //     if (currentImage < 0) {
+        //         currentImage = imagesSource.length - 1
+        //     }
+
+        //     document.getElementById('image').src = imagesSource[currentImage]
+
+
+
+        // }
+
+
+        // console.log("heloooooo")
+
+
+
+})();
